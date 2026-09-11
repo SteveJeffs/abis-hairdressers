add197259674e386eab9b995f40435eed7d291f0
Steve Jeffs <topjeffs@hotmail.com>
2026-09-11 12:39:44 +0100

    JS Menu

---
+++ script.js
@@ -0,0 +1,10 @@
+const menuBtn = document.getElementById("menuBtn");
+const panel = document.getElementById("panel");
+
+menuBtn.addEventListener("click", function () {
+    const isOpen = panel.classList.toggle("showing");
+    menuBtn.setAttribute("aria-expanded", String(isOpen))
+});
+
+panel.classList.toggle("showing");
+
