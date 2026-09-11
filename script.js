85d619a011ec35f32cfa9cc84306a06c0421bbcd
Steve Jeffs <topjeffs@hotmail.com>
2026-09-11 13:02:18 +0100

    Close Btn

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const menuBtn = document.getElementById("menuBtn");
+const closeBtn = document.getElementById("closeBtn");
 const panel = document.getElementById("panel");
 
 menuBtn.addEventListener("click", function () {
@@ -6,3 +7,7 @@ menuBtn.addEventListener("click", function () {
     menuBtn.setAttribute("aria-expanded", String(isOpen));
 });
 
+closeBtn.addEventListener("click", function () {
+    panel.classList.remove("showing");
+    menuBtn.setAttribute("aria-expanded", "false");
+});
